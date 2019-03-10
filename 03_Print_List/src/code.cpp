5b0c7db220b0f4e5f962139ba5e6eada14cd0cc7
BetterYubingDeng <31949230+BetterYubingDeng@users.noreply.github.com>
2019-03-10 17:14:55 +0800

    Update code.cpp

--- 03_Print_List/src/code.cpp
+++ 03_Print_List/src/code.cpp
@@ -1,2 +1,27 @@
-#include <iostream>
-using namespace std;
+struct ListNode
+{
+    int val;
+    struct ListNode *next;
+    ListNode(int x): val(x),next(NULL)
+    {
+    }
+};
+
+class Solution 
+{
+public:
+    vector<int> printListFromTailToHead(ListNode* head)
+    {
+        vector<int> value_return;
+        if(head != NULL)
+        {
+            value_return.insert(value_return.begin(),head->val);
+            while ( head -> next != NULL)
+            {
+               value_return.insert(value_return.begin(),head->next->val);
+               head = head->next;
+            }
+        }
+        return value_return;
+    }
+};
